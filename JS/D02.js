13151f41cde95a575d767d9ebb97d8a1e229495a
CrysPy-SAM <ssmishra9918@gmail.com>
2026-04-06 11:40:13 +0530

    functions

---
+++ JS/D02.js
@@ -0,0 +1,21 @@
+// age = 65; 
+// console.log(age);
+
+// let age = 24;
+// console.log(age);
+
+// age = 45; 
+// console.log(age);
+
+//function
+
+function sayHello(){
+
+}
+sayHello();
+
+function greetuser(x) {
+    console.log('Hey', x);
+}
+
+greetuser('satyam');
