66fb118cfcb89a053097a78c306f4334633f0dbc
CrysPy-SAM <ssmishra9918@gmail.com>
2026-04-08 10:35:28 +0530

    D03

--- JS/D02.js
+++ JS/D02.js
@@ -1,21 +1,28 @@
-// age = 65; 
-// console.log(age);
+// // // age = 65; 
+// // // console.log(age);
 
-// let age = 24;
-// console.log(age);
+// // // let age = 24;
+// // // console.log(age);
 
-// age = 45; 
-// console.log(age);
+// // // age = 45; 
+// // // console.log(age);
 
-//function
+// // //function
 
-function sayHello(){
+// // function sayHello(){
 
-}
-sayHello();
+// // }
+// // sayHello();
 
-function greetuser(x) {
-    console.log('Hey', x);
-}
+// // function greetuser(x) {
+// //     console.log('Hey', x);
+// // }
 
-greetuser('satyam');
+// // greetuser('satyam');
+
+// arraow function 
+
+
+const isAllowdtovote = (age) => age >= 18;
+const isuserAllowedtoOpenBankAccount = (age, minBalance) => age >= 18  && minBalance >= 5000;
+     console.log(isAllowdtovote(23, 6000));
