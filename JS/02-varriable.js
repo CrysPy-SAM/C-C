8e0434bc8db8d45804c5203c9e57e066740ef784
CrysPy-SAM <ssmishra9918@gmail.com>
2026-04-08 11:37:45 +0530

    var,const,let

---
+++ JS/02-varriable.js
@@ -0,0 +1,44 @@
+var shipNmae = " the Amber"
+console.log("Shipname: ", shipName)
+
+let crewCounnt = 12 
+console.log("crew count:" , crewCount )
+crewCount= 14
+
+const capatainName = "jack Sparrow"
+console.log("Captain Name:", capatainName)
+capatainName = "Satyam"
+
+if(true){
+    var leakytreasure= "gold Coins"
+}
+
+for (var i = 0; i< 10 ; i++){
+
+}
+
+for ( let j = 0; i<10; i++){
+
+}
+
+console.log (leakytreasure);
+
+let shipSpeed= 22
+let _privatelog = "secreat"
+let MONGODB_URL = ""
+let $name = "satyam"
+
+const treasureChest = {
+    gold : 100,
+    rubies : 50,
+    maps : 2
+}
+
+treasureChest.gold = 150 ;
+treasureChest = {gold: 50 };
+
+const crewRosters = ["Alok", "Abhinav", "Tanishak"]
+crewRosters.push ("viraj");
+crewRosters[0] = "ram";
+
+crewRosters = ["someone"];
