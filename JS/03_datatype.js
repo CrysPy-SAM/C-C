b840479b37ad35a8fd87f5e8bd1ab86565f17af4
CrysPy-SAM <ssmishra9918@gmail.com>
2026-04-17 11:15:09 +0530

    dataTypes

---
+++ JS/03_datatype.js
@@ -0,0 +1,9 @@
+const weaponename = "Flame Sword";
+console.log("Weapon:",weaponename,"| type:", typeof weaponename);
+
+const attackPower  = 75
+const attackUpgrade = 1.5  
+
+
+console.log(typeof attackPower)
+console.log(typeof attackUpgrade)
